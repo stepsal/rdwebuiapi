ae51139a42713697a3b7472e3c09fbcb4ec8e5f8
stephen salmon <stephensalmon.mayo@gmail.com>
2023-07-05 09:06:29 +0100

    project init

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 """ See:
-https://github.com/mix1009/sdwebuiapi
+https://github.com/stepsal/rdwebuiapi/"
 """
 
 # Always prefer setuptools over distutils
@@ -10,14 +10,14 @@ here = pathlib.Path(__file__).parent.resolve()
 long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
-    name="webuiapi",
-    version="0.9.3",
-    description="Python API client for AUTOMATIC1111/stable-diffusion-webui",
-    url="https://github.com/mix1009/sdwebuiapi",
-    author="ChunKoo Park",
-    author_email="mix1009@gmail.com",
-    keywords="stable-diffuion-webui, AUTOMATIC1111, stable-diffusion, api",
-    packages=["webuiapi"],
+    name="rdwebuiapi",
+    version="0.0.1",
+    description="Python API client for Run Diffusion installation of AUTOMATIC1111/stable-diffusion-webui",
+    url="https://github.com/stepsal/rdwebuiapi/",
+    author="Stephen Salmon",
+    author_email="stephensalmon.mayo@gmail.com",
+    keywords="stable-diffuion-webui, AUTOMATIC1111, stable-diffusion, api, Run Diffusion",
+    packages=["rdwebuiapi"],
     #packages=find_packages(),
     python_requires=">=3.7, <4",
     install_requires=['requests',
